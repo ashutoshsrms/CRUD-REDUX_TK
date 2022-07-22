73e41b53b099aca8abefe47e9275a85a9a1bfcea
ashutoshsrms <m.ashutosh.1642@gmail.com>
2022-07-22 18:47:18 +0530

    first commit

---
+++ src/redux/features/PostSlice.js
@@ -0,0 +1,180 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import  axios  from "axios";
+
+// export const getPost = createAsyncThunk("post/getPost", async ({ id }) => {
+//   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
+//     res.json()
+//   );
+// });
+
+export const getPost = createAsyncThunk("post/getPost", async ({ id }) => {
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+});
+
+
+
+// export const deletePost = createAsyncThunk(
+//   "post/deletePost",
+//   async ({ id }) => {
+//     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+//       method: "DELETE",
+//     }).then((res) => res.json());
+//   }
+// );
+
+export const deletePost = createAsyncThunk(
+  "post/deletePost",
+  async ({ id }) => {
+    return axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    }).then((res) => res.json());
+  }
+);
+
+
+// export const createPost = createAsyncThunk(
+//   "post/createPost",
+//   async ({ values }) => {
+//     return fetch(`https://jsonplaceholder.typicode.com/posts`, {
+//       method: "POST",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         title: values.title,
+//         body: values.body,
+//       }),
+//     }).then((res) => res.json());
+//   }
+// );
+
+
+
+export const createPost = createAsyncThunk(
+  "post/createPost",
+  async ({ values }) => {
+    try{
+    const res = await axios.post(`https://jsonplaceholder.typicode.com/posts`, {
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+        body: JSON.stringify({
+        title: values.title,
+        body: values.body,
+      }),
+    });
+    return res.data;
+  }catch(err){
+    return err
+  }
+}
+);
+
+
+// export const updatePost = createAsyncThunk(
+//   "post/updatePost",
+//   async ({ id, title, body }) => {
+//     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+//       method: "PUT",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         title,
+//         body,
+//       }),
+//     }).then((res) => res.json());
+//   }
+// );
+
+
+export const updatePost = createAsyncThunk(
+  "post/updatePost",
+  async ({ id, title, body }) => {
+    const res= await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        body,
+      }),
+    })
+    return res.data;
+  }
+);
+
+
+
+const PostSlice = createSlice({
+  name: "post",
+  initialState: {
+    post: [],
+    loading: false,
+    error: null,
+    body: "",
+    edit: false,
+  },
+  reducers: {
+    setEdit: (state, action) => {
+      state.edit = action.payload.edit;
+      state.body = action.payload.body;
+    },
+  },
+  extraReducers: {
+    [getPost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [getPost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [getPost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [deletePost.pending]: (state) => {
+      state.loading = true;
+    },
+    [deletePost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [deletePost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [createPost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [createPost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [createPost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [updatePost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [updatePost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [updatePost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+export const { setEdit } = PostSlice.actions;
+export default PostSlice.reducer;
