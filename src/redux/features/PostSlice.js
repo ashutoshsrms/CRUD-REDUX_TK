131a7841cfae077a0ba9a44de72552e7d4d03557
Ashutosh Maurya <90456156+ashutoshsrms@users.noreply.github.com>
2022-07-26 13:04:14 +0530

    Update PostSlice.js

--- src/redux/features/PostSlice.js
+++ src/redux/features/PostSlice.js
@@ -1,11 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import  axios  from "axios";
 
-// export const getPost = createAsyncThunk("post/getPost", async ({ id }) => {
-//   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
-//     res.json()
-//   );
-// });
+
 
 export const getPost = createAsyncThunk("post/getPost", async ({ id }) => {
     try {
@@ -17,16 +13,6 @@ export const getPost = createAsyncThunk("post/getPost", async ({ id }) => {
 });
 
 
-
-// export const deletePost = createAsyncThunk(
-//   "post/deletePost",
-//   async ({ id }) => {
-//     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-//       method: "DELETE",
-//     }).then((res) => res.json());
-//   }
-// );
-
 export const deletePost = createAsyncThunk(
   "post/deletePost",
   async ({ id }) => {
@@ -35,23 +21,6 @@ export const deletePost = createAsyncThunk(
   }
 );
 
-
-// export const createPost = createAsyncThunk(
-//   "post/createPost",
-//   async ({ values }) => {
-//     return fetch(`https://jsonplaceholder.typicode.com/posts`, {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         title: values.title,
-//         body: values.body,
-//       }),
-//     }).then((res) => res.json());
-//   }
-// );
 
 
 
@@ -75,24 +44,6 @@ export const createPost = createAsyncThunk(
   }
 }
 );
-
-
-// export const updatePost = createAsyncThunk(
-//   "post/updatePost",
-//   async ({ id, title, body }) => {
-//     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-//       method: "PUT",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         title,
-//         body,
-//       }),
-//     }).then((res) => res.json());
-//   }
-// );
 
 
 export const updatePost = createAsyncThunk(
